1d6c98aaa742aa2bd3f6c38c970faa2811e238f7
jhamPac <cloud@kevia.me>
2020-05-11 08:34:23 -0700

    added the error checks back in

--- main.go
+++ main.go
@@ -7,16 +7,24 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-func parseWiki(url string) string {
-	resp, _ := http.Get(url)
+func parseWiki(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("GET request failed: %v", err)
+	}
 	defer resp.Body.Close()
 
-	doc, _ := goquery.NewDocumentFromReader(resp.Body)
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("goquery could not read the body: %v", err)
+	}
 
 	var tracks []string
 	var track string
 
 	tracks = append(tracks, "artist,song\n")
+
+	doc.Find(".div-col").Each
 }
 
 func main() {
