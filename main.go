f6629c886d33f73ee13db90f781a97bda55117ba
jhamPac <cloud@kevia.me>
2020-05-10 19:45:23 -0700

    easy request func

--- main.go
+++ main.go
@@ -1,6 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func makeRequest(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", nil
+	}
+
+	return string(body), nil
+}
 
 func main() {
 	fmt.Println("This is how you Makefile")
